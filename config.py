e20e9fe2b624f45be52fb05ea15ec3b8535cf1a2
MarioBassem <mariobassem12@gmail.com>
2021-03-01 22:47:52 +0200

    Update config.py

--- config.py
+++ config.py
@@ -10,5 +10,5 @@ DEBUG = True
 
 
 # TODO IMPLEMENT DATABASE URL
-SQLALCHEMY_DATABASE_URI = 'postgres://mario@localhost:5432/fyyur'
+SQLALCHEMY_DATABASE_URI = 'postgres://kxkiemgvbsmlyj:4f46995629632baf75e29ea130554cc7ca014b56063a580acb9779aaa166af92@ec2-3-87-180-131.compute-1.amazonaws.com:5432/d84jodtt3hs0v3'
 SQLALCHEMY_TRACK_MODIFICATIONS  = False
